768e7b52f2837d21bc2f18c857929ea909d190d3
Nhu Minh <minhnnit>
2019-01-25 00:51:05 +0700

    edit css header and form search results and results-amp blade

--- resources/views/elements/header.blade.php
+++ resources/views/elements/header.blade.php
@@ -1,22 +1,22 @@
 @if(isset($hiddenSearchHeader) && $hiddenSearchHeader === 0)
-<div class="home-box-search head">
-    <div class="col-xs-12">
-        <div class="col-md-8 con-header">
-            <div class="col-md-3">
-                <a href="{{ url('/') }}">
-                <img src="{{ asset('/images/logo.png') }}" class="logo">
-                </a>
-            </div>
-            <div class="input-group search-box head col-md-9">
-                <form id="frmSearch" autocomplete="off" style="display: inherit">
-                <input type="text" class="form-control input-search" placeholder="" id="q">
-                <span class="input-group-btn">
+    <div class="home-box-search head">
+        <div class="col-xs-12">
+            <div class="col-md-8 col-xs-12 con-header">
+                <div class="col-md-3 col-sm-12 centered ">
+                    <a href="{{ url('/') }}">
+                        <img src="{{ asset('/images/logo.png') }}" class="logo">
+                    </a>
+                </div>
+                <div class="input-group search-box head col-md-9 col-sm-12 col-xs-12 col-sm-offset-1 col-xs-offset-1">
+                    <form id="frmSearch" class="frmSearch-home" autocomplete="off" style="display: inherit">
+                        <input type="text" class="form-control input-search" placeholder="" id="q">
+                        <span class="input-group-btn">
                 <button class="btn btn-default border-right-radius" type="submit" id="btnSearch"><i class="fas fa-search"></i></button>
                 </span>
-                </form>
-            </span>
+                    </form>
+                    </span>
+                </div>
             </div>
         </div>
     </div>
-</div>
 @endif
