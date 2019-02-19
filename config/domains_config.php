93e1fcbe5e08d09b72b27ef13d56e54c9798e72a
haidang9x <hopthucuahaidang@gmail.com>
2019-02-19 22:56:50 +0700

    edit config
    edit config
    
    reset domain config
    
    reset domain config
    
    reset domain config

---
+++ config/domains_config.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+	'localhost' => [
+		'enable_search' => 1,
+		'template' => 'getsetcoupon'
+	],
+	'getsetcoupon.com' => [
+		'enable_search' => 1,
+		'template' => 'getsetcoupon'
+	],
+];
+
+?>
